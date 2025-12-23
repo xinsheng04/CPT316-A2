826b3efc2a54025bac7206fa7ca8bfefa4d2a08b
xinsheng04 <freddy161204@gmail.com>
2025-12-23 13:28:13 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Welcome to the digit classifier application!
+## This application accepts a handwritten digit and predicts it's value using a keras NN
+## It is created to satisfy the requirements of CPT316 - A2
+
+Tech stack: Node-Red, Python, Keras, Scikit-learn, tensorflow
+
+Execution instructions:
+---Activating Node-red---
+1. Get the root directory of this project, for example, C:\Users\Asus\OneDrive\Desktop\myProjects\Y3S1\CPT 316\a2
+2. In your PowerShell cmd (Windows Powershell) paste cd [your-root-directory] to switch to that root directory
+- For example, cd 'C:\Users\Asus\OneDrive\Desktop\myProjects\Y3S1\CPT 316\a2'
+3. In that very same directory, run node-red and open Node-red in the browser
+---Activating the predictor function---
+4. Open another cmd prompt (Windows PowerShell) in the same root directory. For example, you can do it in VSCode
+5. run python predict.py
+6. Wait for the words " * Debugger is active! * Debugger PIN: 361-710-501" to appear in the cmd prompt
+7. Open the ui in the dashboard
+8. Draw and predict away!
+
+Note: Digit prediction is not 100% accurate.
---
+++ predict.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from tensorflow import keras
+
+app = Flask(__name__)
+r'''
+-----------------------------------------------
+Paste the following into PowerShell before running node-red:
+cd 'C:\Users\Asus\OneDrive\Desktop\myProjects\Y3S1\CPT 316\a2'
+-----------------------------------------------
+'''
+# Load model once - stays in RAM
+print("---Keras Loads Model---")
+model = keras.models.load_model("digit_classifier.keras")
+print("---Model Loaded---")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json(force=True)
+        pixels = np.array(data, dtype=np.float32).reshape(1, 784)
+        
+        preds = model.predict(pixels, verbose=0)
+        
+        # CRITICAL: Convert numpy types to native Python types
+        digit = int(np.argmax(preds))
+        confidence = round(float(np.max(preds)), 5)
+        
+        # This is what Node-RED is waiting for
+        return jsonify({
+            "digit": digit,
+            "confidence": confidence
+        })
+    except Exception as e:
+        print(f"Python Error: {e}")
+        return jsonify({"error": str(e)}), 500
+    
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
